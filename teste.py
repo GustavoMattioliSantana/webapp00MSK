fba9c3fbb2250ea7a70994caccbfb4170a7dfcbc
GustavoMattioliSantana <164009083+GustavoMattioliSantana@users.noreply.github.com>
2024-05-14 14:06:27 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,6 +1,17 @@
 import streamlit as st
 
-st.title("Site muito foda")
+page_bg_img = '''
+<style>
+body {
+background-image: url("https://images.unsplash.com/photo-1542281286-9e0a16bb7366");
+background-size: cover;
+}
+<h1> Teste do texto em html
+</style>
+'''
+
+st.markdown(page_bg_img, unsafe_allow_html=True)
+st.title("Site top")
 st.write("teste dos sites locos do paranaue")
 st.image('dog.jpg', caption='dog fofo')
 
