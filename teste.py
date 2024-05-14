6dcd1cf45dd9da352d6dee4c2432c91126b3c6d5
GustavoMattioliSantana <164009083+GustavoMattioliSantana@users.noreply.github.com>
2024-05-14 14:13:04 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -8,8 +8,8 @@ background-size: cover;
 }
 </style>
 '''
-
 st.markdown(page_bg_img, unsafe_allow_html=True)
+
 st.title("Site top")
 st.write("teste dos sites locos do paranaue")
 st.image('dog.jpg', caption='dog fofo')
