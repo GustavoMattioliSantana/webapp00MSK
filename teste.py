d6986e485137b56fcd690774e790a01489e149c8
GustavoMattioliSantana <164009083+GustavoMattioliSantana@users.noreply.github.com>
2024-05-14 14:20:02 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,7 +1,7 @@
 import base64
 import streamlit as st
 
- def get_base64(bin_file):
+def get_base64(bin_file):
     with open(bin_file, 'rb') as f:
         data = f.read()
     return base64.b64encode(data).decode()
