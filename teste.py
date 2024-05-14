52ac330c131ef0194e1b06bafacaa609b8052c5b
GustavoMattioliSantana <164009083+GustavoMattioliSantana@users.noreply.github.com>
2024-05-14 13:49:48 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -3,3 +3,9 @@ import streamlit as st
 st.title("Site muito foda")
 st.write("teste dos sites locos do paranaue")
 st.image('dog.jpg', caption='dog fofo')
+
+st.header('st.button')
+if st.button('Say hello'):
+st.write('Why hello there')
+else:
+st.write('Goodbye')
