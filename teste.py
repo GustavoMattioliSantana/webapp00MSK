d9f2af7eede88040be17cb2a5e9e55f893d9a023
GustavoMattioliSantana <164009083+GustavoMattioliSantana@users.noreply.github.com>
2024-05-14 14:11:47 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -3,10 +3,9 @@ import streamlit as st
 page_bg_img = '''
 <style>
 body {
-background-image: url("https://images.unsplash.com/photo-1542281286-9e0a16bb7366");
+background-image: src("mont.png");
 background-size: cover;
 }
-<h1> Teste do texto em html</h1>
 </style>
 '''
 
@@ -15,7 +14,7 @@ st.title("Site top")
 st.write("teste dos sites locos do paranaue")
 st.image('dog.jpg', caption='dog fofo')
 
-st.header('st.button')
+st.header('botao')
 if st.button('Say hello'):
   st.write('Why hello there')
 else:
